b1cab20049ce8d78956dd0eac3329aea37bfb4b6
Ankur Satyarthi <ankurindiaofficial@gmail.com>
2023-01-15 22:07:31 +0530

    patients data

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Diabetic-Patients-Analysis-and-Predictions
-Diabetic Patients Analysis and Predictions
+
+**Question 1 :** How many *diabetic patients* data do we have ?
+
+**Answer :** We total of *768* patients data.
--- diabetic_patient_analysis.jl
+++ diabetic_patient_analysis.jl
@@ -1,2 +1,6 @@
 using CSV,DataFrames,GLM,CategoricalArrays,Plots,StatsPlots,TypedTables 
-diabetes=DataFrame(CSV.File("data/diabetes.csv"));describe(diabetes);first(diabetes,5);vscodedisplay(diabetes)
+diabetes=DataFrame(CSV.File("data/diabetes.csv"));describe(diabetes);first(diabetes,5);describe(diabetes,extrema=>:extrema)
+filter(:Outcome=>==(0),diabetes)
+filter(:Outcome=>==(1),diabetes)
+describe(diabetes)
+diabetes
